d9169da4eddc4ac7a444cdff10f54fc7b9e1b46b
Julian Cannon <cannonj55@gmail.com>
2020-10-18 01:33:42 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,86 @@
+document.getElementById('issueForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e) {
+  var issueDesc = document.getElementById('issueDesc').value;
+  var issueSeverity = document.getElementById('issueSeverity').value;
+  var issueAssignedTo = document.getElementById('issueAssignedTo').value;
+  var issueId = chance.guid();
+  var issueStatus = 'Open';
+
+  var issue = {
+    id: issueId,
+    description: issueDesc,
+    severity: issueSeverity,
+    assignedTo: issueAssignedTo,
+    status: issueStatus
+  }
+
+  if (localStorage.getItem('issues') == null) {
+    var issues = [];
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  } else {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+  }
+
+  document.getElementById('issueForm').reset();
+
+  getIssues();
+
+  e.preventDefault();
+}
+
+function setStatusClosed(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = 'Closed';
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  getIssues();
+}
+
+function deleteIssue(id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+
+  for (var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem('issues', JSON.stringify(issues));
+
+  getIssues();
+}
+
+function getIssues() {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  var issuesList = document.getElementById('issuesList');
+
+  issuesList.innerHTML = '';
+
+  for (var i = 0; i < issues.length; i++) {
+    var id = issues[i].id;
+    var desc = issues[i].description;
+    var severity = issues[i].severity;
+    var assignedTo = issues[i].assignedTo;
+    var status = issues[i].status;
+
+    issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<h5><span class="label label-info">Currnet Status: ' + status + '</span></h5>'+
+                              '<p>Description: ' + desc + '</p>'+
+                              '<p><span class="glyphicon glyphicon-time">Severity:</span> ' + severity + '</p>'+
+                              '<p><span class="glyphicon glyphicon-user">Assigned To:</span> ' + assignedTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
+  }
+}
